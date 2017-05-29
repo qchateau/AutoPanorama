bc81d85a4ff1ab67515ec9c342eb37878320670d
Quentin C <quentin.chateau@gmail.com>
2017-05-29 08:49:48 +0200

    Added missing file, improved video support

--- src/videopreprocessor.cpp
+++ src/videopreprocessor.cpp
@@ -1,4 +1,5 @@
 #include "videopreprocessor.h"
+#include <QtDebug>
 
 using namespace  cv;
 
@@ -19,12 +20,19 @@ std::vector<Mat> VideoPreprocessor::evenTimeSpace(int nr)
         interval = (frame_count-1) / (nr-1);
     else
         interval = frame_count;
+    interval = interval >= 1 ? interval : 1;
+
     for (double idx=0; idx < frame_count; idx+=interval)
     {
+        int nr_try = 0;
         Mat img;
         capture.set(CAP_PROP_POS_FRAMES, std::round(idx));
-        capture.read(img);
-        images.push_back(img);
+        while (img.empty() && nr_try++ < interval)
+            capture.read(img);
+        if (img.empty())
+            qDebug() << "Could not find a non empty image at" << idx << ": skipping";
+        else
+            images.push_back(img);
     }
     return images;
 }
