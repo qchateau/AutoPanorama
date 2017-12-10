7dacfc8cdd05408720485679eef14ac8d87b59ba
Quentin C <quentin.chateau@gmail.com>
2017-12-10 21:50:05 +0800

    Added scripts for linux

---
+++ scripts/compile_autopanorama.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT="$DIR/.."
+BUILD_DIR="$DIR/../build/autopanorama/"
+CORES=`nproc --all`
+
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+qmake $ROOT/AutoPanorama.pro
+make -j$CORES
---
+++ scripts/linux_build_and_deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BUILD_DIR="$DIR/../build/autopanorama/"
+
+$DIR/compile_opencv.sh
+$DIR/compile_autopanorama.sh
+$DIR/linux_deploy.sh $BUILD_DIR/autopanorama
