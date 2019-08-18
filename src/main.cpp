5e07f403a7727b13d4097270255dbf09b7c53d2a
Quentin Chateau <quentin.chateau@gmail.com>
2019-08-18 11:30:32 +0200

    appveyor CI, fix style

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+#ifdef _WIN32
+    a.setStyle(QStyleFactory::create("fusion"));
+#endif
+
     MainWindow w;
     w.show();
 
